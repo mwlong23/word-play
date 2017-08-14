653cd477e68d0dc9933617ac730719f8b80e6e8a
Bably Kumari and Mitchell Long <bably@>
2017-08-14 11:22:21 -0700

    scramble sentence

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
+  $("#userInput").submit(function(event){
+  event.preventDefault();
+  var words = $("input#inputSentence").val().split(" ");
 
+  var min3char = words.map(function(word) {
+    if(word.length >= 3){
+      return word;
+    }
+  });
+
+  var result = min3char.join(" ");
+  var reverse = min3char.reverse().join(" ");
+  $("#result1").text(result);
+  $("#result2").text(reverse);
+  $(".well").show();
+  });
 });
